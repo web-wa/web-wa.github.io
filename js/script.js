20c3c83e7223f1e16b0fbb2c60d11608bd059997
web-wa <wahyutrysaputra123@gmail.com>
2024-07-06 07:11:41 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// clear form before unload
+
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+  form.reset();
+  }
+};
